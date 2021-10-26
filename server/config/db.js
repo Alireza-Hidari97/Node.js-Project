c1843f98be517d32e72b74cf71b5f0b598be0557
Alireza Haidari <m.ghorbat@yahoo.com>
2021-10-26 18:04:51 -0400

    updated UrI to work with mongo db atlat

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,6 @@ const { db } = require("../models/contact")
 
 module.exports =
 {
- "URI": "mongodb://localhost/contacts"
+// "URI": "mongodb://localhost/contacts"
+"URI": "mongodb+srv://Alireza:E6tZnDt5ZMWWicY9@mongodbserver.r81hq.mongodb.net/contacts"
 }
